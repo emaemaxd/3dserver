42bc79c11e000cde53ec4c20de6b02f3451dbf23
emaemaxd <baumname@hotmail.com>
2022-12-13 20:46:56 +0100

    changed returntype to response in getAllExhibitions

--- three-d-portfolio-server/src/main/java/org/threeDPortfolioGallery/resource/ExhibitionResource.java
+++ three-d-portfolio-server/src/main/java/org/threeDPortfolioGallery/resource/ExhibitionResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("api/exhibitions")
@@ -28,9 +29,12 @@ public class ExhibitionResource {
     @GET
     @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Exhibition> getAllExhibitions(){
-
-        return exhibitionRepo.listAll();
+    public Response getAllExhibitions(){
+        List<Exhibition> exhibitionList = exhibitionRepo.listAll();
+        if(exhibitionList.isEmpty()){
+            return Response.noContent().build();
+        } else {
+            return Response.ok().entity(exhibitionList).build();
+        }
     }
-
 }
